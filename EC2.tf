b4f3b5c52f557ed328d333c9a7794c0a070080f9
kola <wilfobsinvestment@gmail.com>
2022-08-12 00:55:02 -0400

    terraform code

---
+++ EC2.tf
@@ -0,0 +1,13 @@
+
+
+
+resource "aws_instance" "terraform-demo" {
+    ami = var.ami
+    instance_type = var.instance-type
+    key_name = var.key-pair
+    tags = {
+        Name = "jenkins"
+        Env = "Dev"
+    }    
+}    
+
